afa755b73d7785d6b7f0f3733cd4edfb47a5b40e
Aleksander Moloniewicz <aleksander@direktpoint.pl>
2017-07-27 14:52:39 +0200

    set up config

---
+++ server/config/config.js
@@ -0,0 +1,13 @@
+/**
+ * Created by SAMSUNG on 27.07.2017.
+ */
+const env = process.env.NODE_ENV || 'development';
+console.log(' env *****', env);
+
+if (env === 'development') {
+  process.env.PORT = 3000;
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+} else if (env === 'test') {
+  process.env.PORT = 3000;
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+}
