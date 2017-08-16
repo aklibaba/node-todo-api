8a00c1594b179aee4946b2e3b93ffa04ad08940a
Aleksander Moloniewicz <aleksander@direktpoint.pl>
2017-08-16 14:57:23 +0200

    fix PORT bug

--- server/config/config.js
+++ server/config/config.js
@@ -3,13 +3,13 @@
  */
 const env = process.env.NODE_ENV || 'development';
 console.log(' env *****', env);
-process.env.PORT = 3000;
 
-if (env === 'development' || env === 'test') {
+if ( env === 'development' || env === 'test' ) {
+  process.env.PORT = 3000;
   const config = require('./config.json');
-  const envConfig = config[env];
+  const envConfig = config[ env ];
 
-  Object.keys(envConfig).forEach( key => {
-    process.env[key] = envConfig[key];
+  Object.keys(envConfig).forEach(key => {
+    process.env[ key ] = envConfig[ key ];
   })
 }
